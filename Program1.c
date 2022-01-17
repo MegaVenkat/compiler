76b8994a2d8edf412051c1c269a5b3eba04bc4af
Priyanshu Tiwari <you@example.com>
2022-01-17 14:17:36 +0530

    Initial

--- Program1.c
+++ Program1.c
@@ -1,66 +1,32 @@
-//Program to accept Regular expressions : abb*cd+
+//Program to accept Regular expressions : abb*
 #include <stdio.h>
 #include <string.h>
-int main()
+int main ()
 {
-    char ch[100];
-    int state = 1, i = 0, t = 0, len;
-    printf("Enter a string : ");
-    gets(ch);
-    len = strlen(ch);
-    
-    if (strlen(ch) < 4 || ch[0] != 'a' || ch[1] != 'b' || ch[strlen(ch) - 1] != 'd')
-        state = 6;
-    else
+  int i,state = 0;
+  char str[100];
+  printf("Enter a string : ");
+  gets(str);
+  if (str[0] != 'a' || strlen(str)<2);
+  else
     {
-        while (i < len && t != 1)
-        {
-            switch (state)
-            {
-            case 1:
-                if (ch[i] == 'a')
-                {
-                    state = 2;
-                    i++;
-                }
-                else
-                    t = 1;
-                break;
-            case 2:
-                while (ch[i] == 'b')
-                {
-                    state = 3;
-                    i++;
-                }
-                break;
-            case 3:
-                if (ch[i] == 'c')
-                {
-                    state = 4;
-                    i++;
-                }
-                else
-                    t = 1;
-                break;
-            case 4:
-                while (ch[i] == 'd')
-                {
-                    state = 5;
-                    i++;
-                }
-                if (ch[i] != 'd')
-                    t = 1;
-                break;
-            default:
-                state = 6;
-                break;
-            }
-        }
+      for (i = 1; i < strlen(str); i++)
+	{
+	  
+	  if (str[i] == 'b')
+	    state = 2;
+	    else
+	    {
+	        state=1;
+	        break;
+	    }
+	}
     }
-    if (state == 5)
-        printf("Accepted");
-    else
-        printf("Rejected");
-
-    return 0;
+  if (state == 2)
+    printf("Accepted " );
+  else
+    printf("Not accepted") ;
+  return 0;
 }
+
+
