b95aebd7fe56bb513b3f7ec738638aa8d3555ac5
Priyanshu Tiwari <you@example.com>
2022-01-17 14:18:33 +0530

    1

--- Program1.c
+++ Program1.c
@@ -1,32 +1,31 @@
 //Program to accept Regular expressions : abb*
 #include <stdio.h>
 #include <string.h>
-int main ()
+int main()
 {
-  int i,state = 0;
-  char str[100];
-  printf("Enter a string : ");
-  gets(str);
-  if (str[0] != 'a' || strlen(str)<2);
-  else
+    int i, state = 0;
+    char str[100];
+    printf("Enter a string : ");
+    gets(str);
+    if (str[0] != 'a' || strlen(str) < 2)
+        ;
+    else
     {
-      for (i = 1; i < strlen(str); i++)
-	{
-	  
-	  if (str[i] == 'b')
-	    state = 2;
-	    else
-	    {
-	        state=1;
-	        break;
-	    }
-	}
+        for (i = 1; i < strlen(str); i++)
+        {
+
+            if (str[i] == 'b')
+                state = 2;
+            else
+            {
+                state = 1;
+                break;
+            }
+        }
     }
-  if (state == 2)
-    printf("Accepted " );
-  else
-    printf("Not accepted") ;
-  return 0;
+    if (state == 2)
+        printf("Accepted ");
+    else
+        printf("Not accepted");
+    return 0;
 }
-
-
